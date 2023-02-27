b587b4fd40a53807e258196b3cf7ce41d56092af
Archer <thivan.nms@gmail.com>
2023-02-27 16:01:07 +0700

    modified assert.py

--- assert.py
+++ assert.py
@@ -1,8 +1,8 @@
 def square(x):
     return x * x
 
-assert square(4) == 16
+assert(square(10) == 100)
 
-assert square(10) == 100
+assert(square(4) == 16)
 
-assert square(11) == 121
+assert(square(8) == 64)
